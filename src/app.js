f88870519f37231a331353fc892956bc91521f16
Omar Elsawi <omarelsawi98@gmail.com>
2021-08-02 00:29:02 +0200

    Add functions that change todos array

--- src/app.js
+++ src/app.js
@@ -1,10 +1,33 @@
 let todos = []
+let ID = 0
 
 const addTodo = () => {
   const todo = document.getElementById('input').value
   document.getElementById('input').innerHTML = ''
+  let thisID = ID++
   todos.concat({
-    name: todo,
+    id: thisID,
+    text: todo,
     finished: false
   })
+}
+
+const deleteTodo = (id) => {
+  todos.filter((todo) => todo.id !== id)
+}
+
+const markAsFinished = (id) => {
+  todos = todos.map((todo) => todo.finished = todo.id === id ? !todo.finished : todo.finished)
+}
+
+const sortByTitle = () => {
+  todos.sort((a, b) => todos[a].text - todos[b].text)
+}
+
+const sortByUnfinished = () => {
+  todos.sort((a, b) => todos[a].finished - todos[b].finished)
+}
+
+const listTodosInUI = ()=>{
+
 }
